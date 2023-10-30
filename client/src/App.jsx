3a2d51fb7c59710741aace11317a98be6d62e514
Farhat Kadiwala <farhat.kadiwala03@gmail.com>
2023-10-30 15:54:49 +0530

    images set

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,21 @@
 import "./App.css";
-import img from "./assets/top-left.svg";
+import topLeft from "./assets/top-left.svg";
+import planet from "./assets/planet.svg";
 
 function App() {
   return (
     <>
       <div className="container">
         <div className="row">
-          <img src={img} className="top-left" alt="image" />
+          <img src={topLeft} className="top-left" alt="image" />
           <div className="col">
             <p>
               <a href={"#"}> find.your.name </a>
             </p>
           </div>
+        </div>
+        <div className="row">
+          <img src={planet} className="planet" />
         </div>
       </div>
     </>
