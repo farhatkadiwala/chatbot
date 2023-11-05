1eb361b9610be4a19925f7d9df24e7262d4bc2b2
Farhat Kadiwala <farhat.kadiwala03@gmail.com>
2023-11-05 22:02:40 +0530

    changed gitignore

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import topLeft from "./assets/top-left.svg";
 import planet from "./assets/planet.svg";
 import { useRef, useState } from "react";
-import useNavigate from 'react-router-dom'
+import useNavigate from "react-router-dom";
 
 function App() {
   const [message, setMessage] = useState("");
